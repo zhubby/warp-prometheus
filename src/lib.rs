276aa1e3c9d7e8094dbe67243b21fd70226456fc
zhubby <yanvalue@gmail.com>
2021-02-12 23:18:29 +0800

    metrics

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ use warp::{Filter,reject::Rejection};
 #[derive(Clone,Debug)]
 pub struct PrometheusMetrics {
     http_requests_total: IntCounterVec,
-    http_requests_1xx_total: IntCounterVec,
-    http_requests_2xx_total: IntCounterVec,
-    http_requests_3xx_total: IntCounterVec,
-    http_requests_4xx_total: IntCounterVec,
-    http_requests_5xx_total: IntCounterVec,
+    // http_requests_1xx_total: IntCounterVec,
+    // http_requests_2xx_total: IntCounterVec,
+    // http_requests_3xx_total: IntCounterVec,
+    // http_requests_4xx_total: IntCounterVec,
+    // http_requests_5xx_total: IntCounterVec,
     http_requests_duration_seconds: HistogramVec,
     registry: Registry,
 }
@@ -24,26 +24,48 @@ impl Default for PrometheusMetrics {
 
 
 impl PrometheusMetrics {
-    pub fn new(registry: Registry) -> Self{
+    pub fn registry(&self) -> &Registry {
+        &self.registry
+    }
+
+    pub fn new() -> Self{
+        let label = ["endpoint", "method", "status"];
         let namespace = "";
+        let registry = Registry::new();
         let http_requests_total_opts =opts!("http_requests_total", "Total number of HTTP requests").namespace(namespace.clone());
         let http_requests_total =
-        IntCounterVec::new(http_requests_total_opts, &["endpoint", "method", "status"]).unwrap();
+        IntCounterVec::new(http_requests_total_opts, &label).unwrap();
         registry .register(Box::new(http_requests_total.clone())).unwrap();
         let http_requests_duration_seconds_opts = opts!("http_requests_duration_seconds","HTTP request duration in seconds for all requests").namespace(namespace);
-        let http_requests_duration_seconds = HistogramVec::new(http_requests_duration_seconds_opts.into(),&["endpoint", "method", "status"]).unwrap();
+        let http_requests_duration_seconds = HistogramVec::new(http_requests_duration_seconds_opts.into(),&label).unwrap();
         PrometheusMetrics {
             registry:registry,
             http_requests_total: http_requests_total,
             http_requests_duration_seconds: http_requests_duration_seconds, 
         }
     }
+
+    fn text(&self)-> String {
+        let mut buffer = vec![];
+        let encoder = TextEncoder::new();
+        encoder
+            .encode(&self.registry.gather(), &mut buffer)
+            .unwrap();
+        String::from_utf8(buffer).unwrap()
+    }
 }
 
-fn metrics() -> PrometheusMetrics {
-    PrometheusMetrics {
 
-    }
+pub fn metrics() -> impl Filter<Extract = PrometheusMetrics, Error = Rejection> + Copy {
+    warp::any().and(warp::any().map(move ||
+        {
+            let p = PrometheusMetrics::new();
+            p.clone()
+        }
+
+    )).and_then(|pm : PrometheusMetrics| async move {
+        Ok::<_, Rejection>(PrometheusMetrics::new())
+    })
 }
 
 #[cfg(test)]
